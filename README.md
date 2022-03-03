ed2110101a4e12d3944598a3cc51fdf3927ebdc4
merhi-odg <sami.merhi@opendatagroup.com>
2022-03-03 15:25:54 -0500

    Initial File Upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MNIST_validator
+MOC Monitoring model to compare/validate output of two different models trained on the MNIST dataset.
---
+++ mnist_validator.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+
+# modelop.metrics
+def metrics(dataframe) -> dict:
+
+    # Basic stats
+    num_records = dataframe.shape[0]
+    matching_rows = dataframe[dataframe["score_tf"] == dataframe["score_sklearn"]]
+    percent_match = np.round(matching_rows.shape[0] / num_records * 100, 2)
+
+    # Confusion Matrix
+    labels_sorted = sorted(pd.Series(dataframe["score_tf"]).unique())
+    conf_mat = confusion_matrix(
+        y_true=dataframe["score_tf"],
+        y_pred=dataframe["score_sklearn"],
+        normalize="all",
+        labels=labels_sorted,
+    ).round(4)
+
+    label_strings = [str(label) for label in labels_sorted]
+
+    # conf_mat is a numpy array. We turn it into array of dicts
+    conf_mat_json = []
+    for idx, _ in enumerate(labels_sorted):
+        conf_mat_json.append(dict(zip(label_strings, conf_mat[idx, :].tolist())))
+
+    # Output
+    results = {
+        "performance": [
+            {
+                "test_name": "Output Comparison",
+                "test_category": "performance",
+                "test_type": "comparison",
+                "test_id": "output_comparison",
+                "values": {
+                    "record_count": num_records,
+                    "pecent_match": percent_match,
+                    "percent_mismatch": 100 - percent_match,
+                    "confusion_matrix": conf_mat_json,
+                },
+            }
+        ]
+    }
+    yield results
